1fc1ea9b1f72d816274fa6e31d2c690c456850bd
9elt <nelt.primary@gmail.com>
2023-01-29 18:37:20 +0100

    compiler get all files

--- src/compiler/compiler.rs
+++ src/compiler/compiler.rs
@@ -1,31 +1,84 @@
-use std::path::PathBuf;
-
 use crate::{config::ECSMConfig, parser::ECSMParser};
+use std::fs;
+use std::io::Result;
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 struct Files {
     html: Vec<PathBuf>,
     css: Vec<PathBuf>,
 }
 
+struct BooleanState {
+    name: String,
+}
+
+struct SelectionState {
+    name: String,
+    keys: Vec<String>,
+}
+
+struct States {
+    boolean: Vec<BooleanState>,
+    selection: Vec<SelectionState>,
+}
+
 pub struct ECSMCompiler {
     config: ECSMConfig,
     parser: ECSMParser,
     files: Files,
+    states: States,
 }
 
 impl ECSMCompiler {
     pub fn new(config: &ECSMConfig) -> Self {
-        Self {
+        let mut compiler = Self {
             config: config.to_owned(),
             parser: ECSMParser::new(),
             files: Files {
                 html: vec![],
                 css: vec![],
             },
+            states: States {
+                boolean: vec![],
+                selection: vec![],
+            },
+        };
+
+        match compiler.get_source_files() {
+            Ok(_) => (),
+            Err(err) => println!("error finding source files {:?}", err),
         }
+
+        compiler
     }
 
     pub fn config(&self) -> &ECSMConfig {
         &self.config
+    }
+
+    pub fn get_source_files(&mut self) -> Result<()> {
+        self.get_files(&self.config.source_path()?)
+    }
+
+    pub fn get_files(&mut self, dir_path: &PathBuf) -> Result<()> {
+        let walker = WalkDir::new(dir_path);
+
+        for entry in walker.into_iter().filter_map(|e| e.ok()) {
+            if entry.path().is_dir() {
+                continue;
+            }
+
+            match entry.path().extension() {
+                Some(ext) => match ext.to_str() {
+                    Some("html") => self.files.html.push(entry.path().to_path_buf()),
+                    Some("css") => self.files.css.push(entry.path().to_path_buf()),
+                    _ => continue,
+                },
+                None => continue,
+            }
+        }
+
+        Ok(())
     }
 }
--- src/compiler/observer.rs
+++ src/compiler/observer.rs
@@ -1,14 +1,16 @@
+use super::compiler::ECSMCompiler;
 use notify::{RecursiveMode, Result};
 use notify_debouncer_mini::{new_debouncer, DebouncedEvent};
 use std::time::Duration;
-use super::compiler::ECSMCompiler;
 
 pub fn watch(compiler: &ECSMCompiler) -> Result<()> {
     let (tx, rx) = std::sync::mpsc::channel();
 
     let mut debouncer = new_debouncer(Duration::from_millis(500), None, tx).unwrap();
 
-    debouncer.watcher().watch(&compiler.config().source_path()?, RecursiveMode::Recursive)?;
+    debouncer
+        .watcher()
+        .watch(&compiler.config().source_path()?, RecursiveMode::Recursive)?;
 
     for res in rx {
         match res {
@@ -22,21 +24,12 @@ pub fn watch(compiler: &ECSMCompiler) -> Result<()> {
 
 fn observer_router(events: Vec<DebouncedEvent>) {
     for event in events {
-        let path_str = event.path.to_string_lossy().to_string();
-
-        let file_or_dir_name = match path_str.split("/").collect::<Vec<&str>>().last() {
-            Some(file_name) => file_name,
-            None => "",
-        };
-
-        let split_name = file_or_dir_name.split(".").collect::<Vec<&str>>();
-
-        let file_ext = match split_name.len() > 1 {
-            true => match split_name.last() {
-                Some(ext) => ext,
-                None => "",
+        let name = match event.path.file_name() {
+            Some(name) => match name.to_str() {
+                Some(name) => name,
+                None => "", // directry check
             },
-            false => "dir",
+            None => "",
         };
 
         if !event.path.exists() {
@@ -45,10 +38,18 @@ fn observer_router(events: Vec<DebouncedEvent>) {
             continue;
         }
 
-        match file_ext {
-            "html" => println!("compiling [html] -> {}", file_or_dir_name),
-            "css" => println!("compiling [css] -> {}", file_or_dir_name),
-            "dir" => println!("compiling [dir] -> {}", file_or_dir_name),
+        let ext = match event.path.extension() {
+            Some(ext) => ext.to_str(),
+            None => match event.path.is_dir() {
+                true => Some("dir"),
+                false => None,
+            },
+        };
+
+        match ext {
+            Some("html") => println!("compiling [html] -> {}", name),
+            Some("css") => println!("compiling [css] -> {}", name),
+            Some("dir") => println!("compiling [dir] -> {}", name),
             _ => (),
         }
     }
