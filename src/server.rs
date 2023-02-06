2ee45b3a74fa044b8e8be6a8d5f04d8a4e5480dd
9elt <nelt.primary@gmail.com>
2023-02-06 23:06:19 +0100

    server refactory

--- src/server.rs
+++ src/server.rs
@@ -3,39 +3,43 @@ use rouille::{Response, Server};
 use std::io::Result;
 
 use std::fs::File;
-use std::path::PathBuf;
 use std::sync::mpsc::Sender;
 use std::thread::JoinHandle;
 
-pub fn create(config: &ECSMConfig) -> Result<(JoinHandle<()>, Sender<()>)> {
+pub fn run(config: &ECSMConfig) -> Result<(JoinHandle<()>, Sender<()>)> {
     let output_path = config.output_path()?;
 
     let server = Server::new(config.server(), move |request| {
         let mut request_url = request.url();
 
-        let is_dir = request_url.clone().pop() == Some("/".chars().next().unwrap());
+        if request_url.chars().next() == "/".chars().next() {
+            request_url.remove(0);
+        }
 
-        request_url.remove(0);
+        let request_path = output_path.join(&request_url);
 
-        let res_path = output_path.join(PathBuf::from(match is_dir {
-            true => format!("{request_url}index.html"),
-            false => request_url,
-        }));
+        let request_path = match request_path.is_dir() {
+            true => request_path.join("index.html"),
+            false => request_path,
+        };
 
-        match File::open(res_path.to_owned()) {
-            Ok(file) => {
-                let mime = match &res_path.extension() {
-                    Some(ext) => match ext.to_string_lossy().as_ref() {
-                        "html" => "text/html; charset=utf8",
-                        "css" => "text/css",
-                        _ => "text/html; charset=utf8",
-                    },
-                    None => "text/html; charset=utf8",
-                };
+        if !request_path.exists() {
+            return Response::html("<h2>404 NOT FOUND</h2>").with_status_code(404);
+        }
 
-                Response::from_file(mime, file).with_status_code(200)
-            }
-            Err(_) => Response::html("404 error.").with_status_code(404),
+        let mime = match request_path.extension() {
+            Some(ext) => match ext.to_string_lossy().as_ref() {
+                "html" => "text/html; charset=utf8",
+                "css" => "text/css",
+                // for my future self... add more mime
+                _ => "text/html",
+            },
+            None => "text/html; charset=utf8",
+        };
+
+        match File::open(request_path.to_owned()) {
+            Ok(file) => Response::from_file(mime, file).with_status_code(200),
+            Err(_) => Response::html("<h2>500 SERVER ERROR</h2>").with_status_code(404),
         }
     })
     .unwrap();
